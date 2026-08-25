e323ebad7774b0275b5d97bdcef38d392c31d33f
Thel Nandar Soe <thelnandar2u2@gmail.com>
2026-08-25 13:40:58 +0630

    Welcome

--- app/summary/page.tsx
+++ app/summary/page.tsx
@@ -1,3 +1,5 @@
 import { AppShell } from '@/components/layout/AppShell'
 import { SummaryContent } from '@/components/summary/SummaryContent'
 export default function SummaryPage(){return <AppShell><SummaryContent/></AppShell>}
+
+///welcome
